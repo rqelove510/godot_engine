ac94a29ff5f51e7fcc5b819613d09ce77ce3f1ef
王振华 <48626883@qq.com>
2025-03-30 01:08:48 +0800

    加解密定版

--- modules/encint/encint.cpp
+++ modules/encint/encint.cpp
@@ -1,36 +1,52 @@
 #include "encint.h"
+//#include <functional>
+//#include <random>
 
+// 初始化全局 key
+int64_t EncInt::_global_key = []() {
+	std::random_device rd;
+	std::mt19937_64 gen(rd());
+	return gen();
+}();
 
+// 预计算旋转后的密钥
+std::vector<int64_t> EncInt::_precomputed_rotated_keys;
+
+void EncInt::_init_precomputed_keys() {
+	constexpr int KEY_BITS = sizeof(_global_key) * 8;
+	_precomputed_rotated_keys.reserve(16);
+	for (int i = 0; i < 16; ++i) {
+		int shift = i % KEY_BITS;
+		int64_t rotated_key = (_global_key << shift) | (_global_key >> (KEY_BITS - shift));
+		_precomputed_rotated_keys.push_back(rotated_key);
+	}
+}
 
 // 加密实现
 void EncInt::_encrypt_value(int64_t p_value) {
+	if (_precomputed_rotated_keys.empty()) {
+		_init_precomputed_keys();
+	}
 	int64_t temp = p_value ^ _m_instance_salt; // 盐值混合
 
-	// 循环位移异或
-	constexpr int KEY_BITS = sizeof(_global_key) * 8;
-	for (int i = 0; i < 8; ++i) {
-		int shift = i % KEY_BITS;
-		int rotated_key = (_global_key << shift) | (_global_key >> (KEY_BITS - shift));
-		temp ^= rotated_key;
+	for (int i = 0; i < 16; ++i) {
+		temp ^= _precomputed_rotated_keys[i];
 	}
 
 	_value = temp ^ (_m_instance_salt & _rdv);
-
-
 }
 
 int64_t EncInt::_decrypt_value() {
+	if (_precomputed_rotated_keys.empty()) {
+		_init_precomputed_keys();
+	}
 	int64_t temp = _value ^ (_m_instance_salt & _rdv);
 
-	constexpr int KEY_BITS = sizeof(_global_key) * 8;
-	for (int i = 8 - 1; i >= 0; --i) {
-		int shift = i % KEY_BITS;
-		int rotated_key = (_global_key << shift) | (_global_key >> (KEY_BITS - shift));
-		temp ^= rotated_key;
+	for (int i = 15; i >= 0; --i) {
+		temp ^= _precomputed_rotated_keys[i];
 	}
 
 	return temp ^ _m_instance_salt; // 逆盐值混合
-
 }
 
 int EncInt::_calculate_checksum(int64_t p_value) const {
@@ -41,36 +57,6 @@ int EncInt::_calculate_checksum(int64_t p_value) const {
 	}
 	return checksum;
 }
-// 默认构造函数
-EncInt::EncInt() :
-		_type("unknow_"), _value(0), _m_instance_salt(0) {}
-
-
-EncInt::EncInt(String type, int64_t init_value, int64_t init_salt) {
-	_type = type;
-
-	if (init_salt == 0) {
-		_m_instance_salt = generate_salt();
-	} else {
-		_m_instance_salt = init_salt;
-	}
-	
-	set_value(init_value);
-	
-}
-
-//void EncInt::_init(const String &p_type, int64_t p_initial_value, int64_t p_salt_value) {
-//	_type = p_type;
-//
-//	if (p_salt_value == 0) {
-//		_m_instance_salt = generate_salt();
-//	} else {
-//		_m_instance_salt = p_salt_value;
-//	}
-//
-//	set_value(p_initial_value);
-//}
-
 
 int64_t EncInt::get_value() {
 	int64_t decrypted = _decrypt_value();
@@ -89,20 +75,48 @@ void EncInt::set_value(int64_t p_value) {
 	emit_signal("value_changed", p_value);
 }
 
-// 静态工厂方法实现
-Object *EncInt::create(const String &p_type, int64_t p_initial_value, int64_t p_salt_value) {
-	return memnew(EncInt(p_type, p_initial_value, p_salt_value));
+// 默认构造函数
+EncInt::EncInt() :
+		_type("unknow_"), _value(0) {
+	_m_instance_salt = generate_salt();
 }
 
+EncInt::EncInt(String type, int64_t init_value) {
+	_type = type;
+	_m_instance_salt = generate_salt();
+
+	set_value(init_value);
+}
+
+// 静态工厂方法实现
+Object *EncInt::create(const String &p_type, int64_t p_initial_value) {
+	return memnew(EncInt(p_type, p_initial_value));
+}
+
+// 生成盐值
+uint32_t EncInt::generate_salt() {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(1, 9);
+	return gen() | (dis(gen) * 0x2000000);
+}
+
+// 静态异或加密方法
+int64_t EncInt::simple_enc(int64_t value) {
+	return (value + 0x3a4d910f) ^ _rdv;
+}
+
+// 静态异或解密方法
+int64_t EncInt::simple_dec(int64_t encrypted_value) {
+	return (encrypted_value ^ _rdv) - 0x3a4d910f;
+}
 
 void EncInt::_bind_methods() {
-		// 绑定 create 方法
+	// 绑定 create 方法
 	ClassDB::bind_static_method(
 			"EncInt",
-			D_METHOD("create", "type", "initial_value", "salt_value"),
+			D_METHOD("create", "type", "initial_value"),
 			&EncInt::create);
-
-	//ClassDB::bind_method(D_METHOD("_init", "type", "initial_value", "salt_value"), &EncInt::_init);
 
 	ClassDB::bind_method(D_METHOD("get_salt_value"), &EncInt::get_salt_value);
 	ClassDB::bind_static_method("EncInt", D_METHOD("generate_salt"), &EncInt::generate_salt);
@@ -116,4 +130,7 @@ void EncInt::_bind_methods() {
 
 	ADD_SIGNAL(MethodInfo("value_changed", PropertyInfo(Variant::INT, "value")));
 	ADD_SIGNAL(MethodInfo("validation_failed", PropertyInfo(Variant::INT, "new_value"), PropertyInfo(Variant::STRING, "type")));
+
+	ClassDB::bind_static_method("EncInt", D_METHOD("simple_enc", "value"), &EncInt::simple_enc);
+	ClassDB::bind_static_method("EncInt", D_METHOD("simple_dec", "encrypted_value"), &EncInt::simple_dec);
 }
--- modules/encint/encint.h
+++ modules/encint/encint.h
@@ -3,11 +3,11 @@
 
 #include "core/crypto/crypto_core.h"
 #include "core/object/object.h"
-
+#include <random>
+#include <vector>
 
 class EncInt : public Object {
 	GDCLASS(EncInt, Object);
-
 
 private:
 	EncInt();
@@ -17,42 +17,40 @@ private:
 	int _checksum;
 
 	uint32_t _m_instance_salt = 0;
-	uint32_t _rdv = 0x1B5AC173; //扰动值
-	int64_t _global_key = 0x1234567890123456;
+	const static uint32_t _rdv = 0x1B5AC173; // 扰动值
+	static int64_t _global_key;
+	static std::vector<int64_t> _precomputed_rotated_keys;
 
-
-    // 私有方法
+	// 私有方法
 	void _encrypt_value(int64_t p_value);
 	int64_t _decrypt_value();
 	int _calculate_checksum(int64_t value) const;
+	void _init_precomputed_keys();
 
 protected:
 	static void _bind_methods();
 
-
 public:
-	// 获取加密后的值
 	int64_t get_value();
-	// 设置加密后的值
 	void set_value(int64_t p_value);
-	// 获取类型
 	String get_type() const { return _type; }
 
-	uint32_t get_salt_value() { return _m_instance_salt; }
+	// 获取盐值
+	uint32_t get_salt_value() const { return _m_instance_salt; }
+	static uint32_t generate_salt();
 
-
-	static uint32_t generate_salt() {
-		Math::randomize();
-		return rand() | ((rand() % 9 + 1) * 0x10000000);
-	}
-	static Object *create(const String &p_type, int64_t p_initial_value, int64_t p_salt_value);
-
+	static Object *create(const String &p_type, int64_t p_initial_value);
 
 	// 默认带参构造函数
-	EncInt(String p_type, int64_t p_initial_value, int64_t salt_value);
+	EncInt(String p_type, int64_t p_initial_value);
 
 	// 析构函数
 	~EncInt() = default;
+
+	// 新增静态异或加密方法
+	static int64_t simple_enc(int64_t value);
+	// 新增静态异或解密方法
+	static int64_t simple_dec(int64_t encrypted_value);
 };
 
 #endif // ENCINT_H
